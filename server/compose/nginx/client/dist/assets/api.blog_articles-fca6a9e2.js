5344bc473849b38ce28e15f18a2c215d7606fd95
mrhuanghs <mrhuanghs@126.com>
2023-12-08 15:55:52 +0800

    fix 头像

---
+++ server/compose/nginx/client/dist/assets/api.blog_articles-fca6a9e2.js
@@ -0,0 +1,1 @@
+import{a as l}from"./server-b9628b69.js";const o=t=>l.post("/v1/blog/blog_articles/client/get-list/",t),s=t=>l.post("/v1/blog/blog_articles/client/get-detail/",t),i=t=>l.post("/v1/blog/blog_articles/client/relate/",t),r=t=>l.post("/v1/blog/blog_articles/client/like/",t);export{s as a,o as b,r as c,i as d};
